5f32b6513d7aea5b4fa83489b53eb59fac76d663
Mo Yakubu <mndayak@gmail.com>
2020-05-22 21:50:46 +0200

    Up limit

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ const createRequest = (input: InputParams, callback: Callback) => {
     return throwError(400, 'Invalid params');
   }
 
-  params = params.replace('limit=1', 'limit=10');
+  params = params.replace('limit=1', 'limit=50');
   const url = `https://${region}.market-api.kaiko.io/${endpoint}?${params}`;
   const headers = {
     'X-Api-Key': process.env.CUBIT_API_KEY,
