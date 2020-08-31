19b05f208fc3e05c6d52e05604ab0cc783a3cf06
Robert Edström <github@legogris.se>
2020-09-01 00:18:10 +0900

    WIP: allow specifying quote
    TODO: filter proxy assets to only those traded to quote or usd (if wuote is usd)

--- src/VWAPDirectExchangeRateClient.ts
+++ src/VWAPDirectExchangeRateClient.ts
@@ -15,13 +15,49 @@ export interface VWAPItem {
   price: Big;
 }
 
-const MAX_QUOTE_ASSETS = process.env.MAX_QUOTE_ASSETS
-  ? parseInt(process.env.MAX_QUOTE_ASSETS, 10)
-  : 5;
-
 export default class VWAPExchangeRateClient implements IPriceClient {
 
-  private static async fetchQuoteAssets(baseAsset: string): Promise<string[]> {
+  public static calculateVWAP(entries: VWAPItem[]): VWAPItem {
+    const volume = entries.reduce(((acc: Big, { volume: v }) => acc.plus(v)), new Big(0));
+    const price = entries.reduce(((acc: Big, { price: p, volume: v }) => acc.plus(p.mul(v))), new Big(0)).div(volume);
+    return {
+      price,
+      volume
+    };
+  }
+
+  private readonly client: MarketDataClient<VWAPEntry>;
+  private readonly maxProxyAssets: number;
+
+  constructor(client: MarketDataClient<VWAPEntry>, maxProxyAssets: number = 5) {
+    this.client = client;
+    this.maxProxyAssets = maxProxyAssets;
+  }
+
+  public async getPrice(baseAsset: string, quoteAsset: string, interval: string) {
+    const proxyAssets = await this.fetchProxyAssets(baseAsset);
+    const constituents = await Promise.all(proxyAssets.map(async proxyAsset =>
+      proxyAsset === quoteAsset
+        ? await this.fetchDirectSpotExchangeRate(baseAsset, quoteAsset, interval)
+        : await this.fetchRate(baseAsset, proxyAsset, quoteAsset, interval)
+    ));
+    const { price } = VWAPExchangeRateClient.calculateVWAP(constituents);
+    return price;
+  }
+
+  public async fetchRate(baseAsset: string, proxyAsset: string, quoteAsset: string, interval: string) {
+    const [baseInProxy, proxyInQuote] = await Promise.all([
+      this.fetchDirectSpotExchangeRate(baseAsset, proxyAsset, interval),
+      this.fetchDirectSpotExchangeRate(proxyAsset, quoteAsset, interval)
+    ]);
+    return {
+      quoteAsset,
+      volume: proxyInQuote.volume.eq(0) ? proxyInQuote.volume : baseInProxy.volume,
+      price: baseInProxy.price.mul(proxyInQuote.price)
+    };
+  }
+
+  private async fetchProxyAssets(baseAsset: string): Promise<string[]> {
     const url = 'https://reference-data-api.kaiko.io/v1/instruments';
     const instrumentsResponse = await axios.get(url, {
       url,
@@ -33,52 +69,8 @@ export default class VWAPExchangeRateClient implements IPriceClient {
     const quoteAssets: string[] = Array.from(new Set<string>(instrumentsResponse.data
       .filter((instrument: any) => !!instrument.quote_asset && instrument.base_asset === baseAsset)
       .map((instrument: any) => instrument.quote_asset)))
-      .slice(0, MAX_QUOTE_ASSETS);
+      .slice(0, this.maxProxyAssets);
     return quoteAssets;
-  }
-  private client: MarketDataClient<VWAPEntry>;
-
-  constructor(client: MarketDataClient<VWAPEntry>) {
-    this.client = client;
-  }
-
-  public async getPrice(base: string, quote: string, interval: string) {
-    // TODO: Add non-USD quote support
-    const price = await this.calculateRate(base, interval);
-    return price;
-  }
-
-  public async calculateRate(baseAsset: string, interval: string) {
-    const quoteAssets = await VWAPExchangeRateClient.fetchQuoteAssets(baseAsset);
-    const constituents = await Promise.all(quoteAssets.map(async quoteAsset =>
-      await this.fetchRate(baseAsset, quoteAsset, interval)
-    ));
-    const { price, volume } = this.calculateVWAP(constituents);
-    return price;
-  }
-
-  public async fetchRate(baseAsset: string, quoteAsset: string, interval: string) {
-    if (quoteAsset === 'usd') {
-      return await this.fetchDirectSpotExchangeRate(baseAsset, quoteAsset, interval);
-    }
-    const [baseQuote, quoteUSD] = await Promise.all([
-      this.fetchDirectSpotExchangeRate(baseAsset, quoteAsset, interval),
-      this.fetchDirectSpotExchangeRate(quoteAsset, 'usd', interval)
-    ]);
-    return {
-      quoteAsset,
-      volume: quoteUSD.volume.eq(0) ? quoteUSD.volume : baseQuote.volume,
-      price: baseQuote.price.mul(quoteUSD.price)
-    };
-  }
-
-  private calculateVWAP(entries: VWAPItem[]): VWAPItem {
-    const volume = entries.reduce(((acc: Big, { volume: v }) => acc.plus(v)), new Big(0));
-    const price = entries.reduce(((acc: Big, { price: p, volume: v }) => acc.plus(p.mul(v))), new Big(0)).div(volume);
-    return {
-      price,
-      volume
-    };
   }
 
   private async fetchDirectSpotExchangeRate(baseAsset: string, quoteAsset: string, interval: string) {
